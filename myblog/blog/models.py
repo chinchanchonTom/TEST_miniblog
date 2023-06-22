4e003ce8a587bdc2e180c314bdf9cf5aa32e279b
chinchanchonTom <132604955+chinchanchonTom@users.noreply.github.com>
2023-06-22 11:31:33 +0300

    Initial commit

---
+++ myblog/blog/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField('Заголовок записи', max_length=100)
+    description = models.TextField('Текст записи')
+    author = models.CharField('Имя автора', max_length=100)
+    date = models.DateField('Дата публикации')
+    img = models.ImageField('Изображение ', upload_to='image/%Y')
+
+    def __str__(self):
+        return f'{self.title},{self.author}'
+
+
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = "Записи"
+
+
+
+    """данные о посте """
+
+class Comments(models.Model):
+    """комментарии"""
+    email = models.EmailField()
+    name = models.CharField('Имя ', max_length= 50 )
+    text_comments = models.TextField('Текст комментария',max_length=2000)
+    post = models.ForeignKey(Post, verbose_name ='Публикация', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name},{self.post}'
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = "комментарии"
+
+
+
+class Likes(models.Model):
+    'Like'
+    ip = models.CharField('IP- address', max_length=100)
+    pos = models.ForeignKey(Post, verbose_name="Публикация", on_delete=models.CASCADE)
+
+
+
+
